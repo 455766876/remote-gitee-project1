7bb1cb2064aaf87d209561804d4f1df2c32f63b4
程威 <455766876@qq.com>
2023-06-11 18:00:11 +0800

    测试

--- test-project/src/main/java/com/cw/MyTest.java
+++ test-project/src/main/java/com/cw/MyTest.java
@@ -8,6 +8,6 @@ package com.cw;
  */
 public class MyTest {
     public static void main(String[] args) {
-//        System.out.println("Hello World!!!");
+        System.out.println("Hello World!!!");
     }
 }
