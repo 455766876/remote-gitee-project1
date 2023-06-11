7622e55471ad2e59ac1579488d9ee43be065975b
程威 <455766876@qq.com>
2023-06-11 18:08:00 +0800

    测试

--- test-project/src/main/java/com/cw/MyTest.java
+++ test-project/src/main/java/com/cw/MyTest.java
@@ -1,13 +1,8 @@
 package com.cw;
 
-/**
- * @title: MyTest
- * @Author Cheng Wei
- * @Date: 2023/6/11 17:55
- * @Version 1.0
- */
+
 public class MyTest {
     public static void main(String[] args) {
-        System.out.println("Hello World!!!");
+        System.out.println("11111111111111111");
     }
 }
